caa4f4efbfcbc94b6f418ee109e51c2d5e06d6a3
saurabh1993 <saurabh1993.kumar@gmail.com>
2020-01-06 19:37:28 +0530

    Update installDependencies.sh

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -11,16 +11,16 @@ echo "${green}Adding Universe repository and updating${reset}"
 apt-add-repository universe
 apt-get update
 echo "${green}Adding dependencies, graphics libraries and tools${reset}"
-apt-get install libssl-dev libusb-1.0-0-dev pkg-config -y -q --no-install-recommends
+DEBIAN_FRONTEND=noninteractive apt-get install libssl-dev libusb-1.0-0-dev pkg-config -y -q --no-install-recommends
 # This is for ccmake
-apt-get install build-essential cmake cmake-curses-gui -y -q --no-install-recommends
+DEBIAN_FRONTEND=noninteractive apt-get install build-essential cmake cmake-curses-gui -y -q --no-install-recommends
 
 # Graphics libraries - for SDK's OpenGL-enabled examples
-apt-get install libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev -y -q --no-install-recommends
+DEBIAN_FRONTEND=noninteractive apt-get install libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev -y -q --no-install-recommends
 
 # QtCreator for development; not required for librealsense core library
-apt-get install qtcreator -y -q --no-install-recommends
+DEBIAN_FRONTEND=noninteractive apt-get install qtcreator -y -q --no-install-recommends
 
 # Add Python 3 support
-apt-get install -y -q --no-install-recommends python3 python3-dev
+DEBIAN_FRONTEND=noninteractive apt-get install -y -q --no-install-recommends python3 python3-dev
 
